1faf6227d63a77620768b028c4e7361f4b65c1d3
Ahmed Bakry <ahmedbakry024@gmail.com>
2026-02-27 23:14:22 +0200

    User Model

---
+++ users/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        DOCTOR = 'DOCTOR', 'Doctor'
+        PATIENT = 'PATIENT', 'Patient'
+        RECEPTIONIST = 'RECEPTIONIST', 'Receptionist'
+        ADMIN = 'ADMIN', 'Admin'
+
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.PATIENT)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        return super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return self.username
+    
+class DoctorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='doctor_profile')
+    specialization = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.specialization}"
+    
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient_profile')
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.username
+    
+class ReceptionistProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='receptionist_profile')
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.username
+
+    def __str__(self):
+        return self.user.username
