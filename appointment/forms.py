0ba90bd9d88b695d6b48125fd98d82cb88b9e407
jannawaelali-creator <janna.wael.ali@gmail.com>
2026-02-28 23:21:45 +0200

    create update status form

--- appointment/forms.py
+++ appointment/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Appointment
 
 class UpdateStatusForm(forms.Form):
     status = forms.ChoiceField(choices=[
@@ -7,4 +8,17 @@ class UpdateStatusForm(forms.Form):
         ('COMPLETED', 'Completed'),
         ('NO_SHOW', 'No Show'),
         ('CANCELLED', 'Cancelled'),
-    ])
+    ],widget=forms.Select(attrs={'class': 'form-control'}))
+
+    reason = forms.CharField(widget=forms.Textarea, required=False)
+
+
+    def clean(self):
+        cleaned_data=super().clean()
+        status=cleaned_data.get("status")
+        reason=cleaned_data.get("reason")
+
+        if status in ['CANCELLED', 'NO_SHOW','COMPLETED'] and not reason:
+            raise forms.ValidationError("you must provide a reason")
+
+        return cleaned_data
